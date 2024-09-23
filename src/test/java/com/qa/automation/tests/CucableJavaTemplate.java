45aabca4f2fa2bd108cde4f3b947040f8038e2d4
sharad-990 <142394329+sharad-990@users.noreply.github.com>
2024-09-23 16:16:32 +0530

    Update CucableJavaTemplate.java

--- src/test/java/com/qa/automation/tests/CucableJavaTemplate.java
+++ src/test/java/com/qa/automation/tests/CucableJavaTemplate.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     // Update the feature path correctly
-    features = {"[CUCABLE:FEATURE]"},
+    features = {"src/test/resources/features/[CUCABLE:FEATURE]"},
     // Point to the package where step definitions are located
     glue = {"com.qa.automation.ui.stepDefinations"},
     // Ensure the Allure plugin is added
